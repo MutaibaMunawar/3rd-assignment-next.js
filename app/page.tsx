94b3d864a78947a502fd601f1cab9db799b637c0
MutaibaMunawar <mutaibachohan8@gmail.com>
2025-01-26 18:03:32 +0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center overflow-hidden"> {/* Ensure no horizontal scroll */}
-      <div className="w-full max-w-6xl mx-auto flex flex-col md:flex-row items-center px-4"> {/* Added padding on mobile to prevent content overflow */}
+    <div className="bg-gray-100 min-h-screen flex items-center overflow-hidden"> 
+      <div className="w-full max-w-6xl mx-auto flex flex-col md:flex-row items-center px-4"> 
         
         {/* Image container */}
         <div className="relative w-full md:w-1/2 mb-4 md:mb-0">
@@ -26,7 +26,7 @@ const Home = () => {
             Your <span className="text-yellow-400">Vision</span> Our <span className="text-yellow-400">Design</span>
           </h1>
           <p className="mt-2 text-gray-800 text-base md:text-sm">
-            'Welcome' Your home is a canvas waiting to be transformed. With our tailored interior design services we bring your vision to life&apos; creating spaces that are both beautiful and functional. Let&apos;s design your dream together.
+            &apos;Welcome&apos; Your home is a canvas waiting to be transformed&apos; With our tailored interior design services we bring your vision to life&apos; creating spaces that are both beautiful and functional &apos; Let&apos;s design your dream together &apos;
           </p>
           <div className="mt-4">
             <span className="block text-gray-800 font-medium">Check our design</span>
