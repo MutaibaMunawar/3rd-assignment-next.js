fb409561335b0877d5dd84e5355e3d17a34bdecc
MutaibaMunawar <mutaibachohan8@gmail.com>
2025-01-26 17:44:50 +0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -4,34 +4,38 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center">
-      <div className="flex w-full max-w-6xl mx-auto">
-        <div className="relative w-1/2">
+    <div className="bg-gray-100 min-h-screen flex items-center overflow-hidden"> {/* Ensure no horizontal scroll */}
+      <div className="w-full max-w-6xl mx-auto flex flex-col md:flex-row items-center px-4"> {/* Added padding on mobile to prevent content overflow */}
+        
+        {/* Image container */}
+        <div className="relative w-full md:w-1/2 mb-4 md:mb-0">
           <Image 
             src="/home1.jpeg" 
             alt="Home Image"
             layout="responsive" 
-            width={1400}  // Increased width
-            height={900}  // Increased height
+            width={1400}  
+            height={900}  
             objectFit="cover" 
             className="rounded-lg shadow-lg"
           />
         </div>
-        <div className="flex-1 p-6 md:p-8 bg-white rounded-lg shadow-lg ml-4">
+        
+        {/* Text container */}
+        <div className="flex-1 p-6 md:p-8 bg-white rounded-lg shadow-lg md:ml-4 text-center md:text-left">
           <h1 className="text-4xl md:text-5xl font-bold text-gray-800">
-            Your <span className="text-yellow-400">Vision</span> Our <span className="text-yellow-400 ">Design</span>
+            Your <span className="text-yellow-400">Vision</span> Our <span className="text-yellow-400">Design</span>
           </h1>
-          <p className="mt-2 text-gray-800 text-base md:text-sm"> {/* Decreased font size */}
-            &apos;Welcome&apos; Your home is a canvas waiting to be transformed. With our tailored interior design services&apos; we bring your vision to life&apos; creating spaces that are both beautiful and functional. Let&apos;s design your dream together.
+          <p className="mt-2 text-gray-800 text-base md:text-sm">
+            'Welcome' Your home is a canvas waiting to be transformed. With our tailored interior design services we bring your vision to life, creating spaces that are both beautiful and functional. Let&apos;s design your dream together.
           </p>
           <div className="mt-4">
             <span className="block text-gray-800 font-medium">Check our design</span>
             <Link 
-        href="/design" 
-        className="mt-6 inline-block px-4 py-2 bg-yellow-500 text-white font-semibold rounded-lg hover:bg-yellow-600 transition"
-      >
-        Explore Our Designs
-      </Link>
+              href="/design" 
+              className="mt-6 inline-block px-4 py-2 bg-yellow-500 text-white font-semibold rounded-lg hover:bg-yellow-600 transition"
+            >
+              Explore Our Designs
+            </Link>
           </div>
         </div>
       </div>
